75abd53fb02dbd71c62d3b117934eb788abf0400
RobWue <robin@thomas-wuebbeling.de>
2016-10-27 10:52:06 +0200

    Activity3 Fehlerbehebung

--- app/src/main/java/mcmrt/event_planer/Activity3.java
+++ app/src/main/java/mcmrt/event_planer/Activity3.java
@@ -1,16 +1,16 @@
 package mcmrt.event_planer;
 
-import android.app.Application;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.FirebaseApp;
 
-public class Activity3 extends Application {
+public class Activity3 extends AppCompatActivity {
 
     @Override
-    public void onCreate() {
-        super.onCreate();
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
         if (!FirebaseApp.getApps(this).isEmpty()) {
 
